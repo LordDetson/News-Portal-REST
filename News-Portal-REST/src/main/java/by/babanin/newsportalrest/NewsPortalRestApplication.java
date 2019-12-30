5405f01a54d21db566e3ab06ef2355792d0043b3
Dzmitry Babanin <babanin.dima@gmail.com>
2019-12-30 16:03:56 +0300

    init: initialization Spring application

---
+++ News-Portal-REST/src/main/java/by/babanin/newsportalrest/NewsPortalRestApplication.java
@@ -0,0 +1,13 @@
+package by.babanin.newsportalrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPortalRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPortalRestApplication.class, args);
+	}
+
+}
---
+++ News-Portal-REST/src/test/java/by/babanin/newsportalrest/NewsPortalRestApplicationTests.java
@@ -0,0 +1,13 @@
+package by.babanin.newsportalrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPortalRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
