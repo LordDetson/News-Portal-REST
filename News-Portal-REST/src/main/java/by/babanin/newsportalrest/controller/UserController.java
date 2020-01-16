d6028665a50fa5d75031ede756c274330c704ae2
Dzmitry Babanin <babanin.dima@gmail.com>
2020-01-16 17:42:41 +0300

    refactor: UserController gives the current user

--- News-Portal-REST/src/main/java/by/babanin/newsportalrest/controller/UserController.java
+++ News-Portal-REST/src/main/java/by/babanin/newsportalrest/controller/UserController.java
@@ -4,9 +4,20 @@ import by.babanin.newsportalrest.model.User;
 import by.babanin.newsportalrest.model.view.ViewUser;
 import by.babanin.newsportalrest.service.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 public class UserController {
@@ -20,5 +31,17 @@ public class UserController {
     @JsonView(ViewUser.NoPassword.class)
     public User registration(@RequestBody User user) {
         return userService.addUser(user);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity currentUser(@AuthenticationPrincipal UserDetails userDetails){
+        Map<Object, Object> model = new HashMap<>();
+        model.put("username", userDetails.getUsername());
+        model.put("roles", userDetails.getAuthorities()
+                .stream()
+                .map(a -> ((GrantedAuthority) a).getAuthority())
+                .collect(toList())
+        );
+        return ok(model);
     }
 }
